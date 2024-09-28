7607d6add6a431021d5033ee5485be80ca898299
Sunny Jacob <sunny@visualise.it>
2024-09-28 11:49:17 +0100

    - updated Header and README.md

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -12,7 +12,7 @@ const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-6xl font-bold">
-        My Custom clone of the popular {" "}
+        Clone of the popular {" "}
         <span className="underline">Notion</span> {" "} Notes App
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl">
